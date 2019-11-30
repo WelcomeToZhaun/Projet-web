12b8a89b3c09764ed31927c49a9f583cc0622782
mboultoureau <contact@mboultoureau.com>
2019-11-30 22:11:57 +0100

    Menu dynamique sur ordinateur actif

---
+++ js/menu.js
@@ -0,0 +1,28 @@
+
+$("header nav ul li, header > div").hover((e) =>
+{
+    // Affichage du sous-menu
+    if (e.currentTarget.classList.length > 0 && e.currentTarget.classList[0].startsWith("sous"))
+    {
+        $("." + e.currentTarget.classList[0]).addClass("displayed");
+        $("." + e.currentTarget.classList[0].substring(4)).addClass("displayed");
+    }
+    else if (e.currentTarget.classList.length > 0)
+    {
+        $(".sous" + e.currentTarget.classList[0]).addClass("displayed");
+        $("." + e.currentTarget.classList[0]).addClass("displayed");
+    }
+}, (e) => 
+{
+    // Désaffichage du sous-menu
+    if ( e.currentTarget.classList.length > 0 && e.currentTarget.classList[0].startsWith("sous"))
+    {
+        $("." + e.currentTarget.classList[0]).removeClass("displayed");
+        $("." + e.currentTarget.classList[0].substring(4)).removeClass("displayed");
+    }
+    else if (e.currentTarget.classList.length > 0)
+    {
+        $(".sous" + e.currentTarget.classList[0]).removeClass("displayed");
+        $("." + e.currentTarget.classList[0]).removeClass("displayed");
+    }
+})
